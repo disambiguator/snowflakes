7de60cc158751cf58f3691621c109a2eb0c05845
Paras Sanghavi <paras@disambiguo.us>
2022-12-21 23:43:06 -0800

    show name

--- pages/walk.tsx
+++ pages/walk.tsx
@@ -8,14 +8,19 @@ import {
   Sphere,
 } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import React, { useEffect, useRef } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import * as THREE from "three";
 import vertexShader from "scene.vert";
 import fragmentShader from "scene.frag";
 import styles from "./scene.module.scss";
 import { airtableList, Model } from "airtableApi";
 import Link from "next/link";
-import useIsMobile from "isMobile";
 
 const TOP = 70;
 const FLOOR = -30;
@@ -33,7 +38,13 @@ type InstancedMesh = Omit<
 const count = 300;
 const rand = (min: number, max: number) => min + Math.random() * (max - min);
 
-const Snowflake = () => {
+const Snowflake = ({
+  onPointerEnter,
+  onPointerLeave,
+}: {
+  onPointerEnter: () => void;
+  onPointerLeave: () => void;
+}) => {
   const ref = useRef<any>(null);
 
   const r = Math.sqrt(Math.random()) * 80;
@@ -49,11 +60,6 @@ const Snowflake = () => {
 
   useFrame((state) => {
     const t = state.clock.getElapsedTime() * speed;
-    // ref.current.rotation.set(
-    //   Math.cos(t / 4) / 2,
-    //   Math.sin(t / 4) / 2,
-    //   Math.cos(t / 1.5) / 2
-    // );
     const instance = ref.current!;
     instance.position.y = TOP - ((position[1] + t) % (TOP - FLOOR));
     instance.position.x = position[0] + Math.sin(t / 10) * xSin;
@@ -64,6 +70,8 @@ const Snowflake = () => {
       ref={ref}
       scale={rand(1, 7)}
       position={position}
+      onPointerEnter={onPointerEnter}
+      onPointerLeave={onPointerLeave}
       rotation={[
         Math.PI * 2 * Math.random(),
         Math.PI * 2 * Math.random(),
@@ -73,12 +81,39 @@ const Snowflake = () => {
   );
 };
 
-const Snow = ({ fields }: { fields: Model[] }) => {
+const Snow = ({
+  fields,
+  setName,
+}: {
+  fields: Model[];
+  setName: (name: string | null) => void;
+}) => {
   const geomRef = useRef<InstancedMesh>(null);
 
-  const snowflakes = new Array(count)
-    .fill(undefined)
-    .map((_, i) => <Snowflake key={i} />);
+  const onPointerEnter = useCallback(
+    (i: number) => () => {
+      setName(fields[i % fields.length].name);
+    },
+    [fields, setName]
+  );
+
+  const onPointerLeave = useCallback(() => {
+    setName(null);
+  }, [setName]);
+
+  const snowflakes = useMemo(
+    () =>
+      new Array(count)
+        .fill(undefined)
+        .map((_, i) => (
+          <Snowflake
+            key={i}
+            onPointerEnter={onPointerEnter(i)}
+            onPointerLeave={onPointerLeave}
+          />
+        )),
+    [onPointerEnter, onPointerLeave]
+  );
 
   useEffect(() => {
     const geom = geomRef.current!.geometry;
@@ -97,12 +132,55 @@ const Snow = ({ fields }: { fields: Model[] }) => {
     }
   }, [fields]);
 
+  const instances = useMemo(() => {
+    return (
+      <Instances ref={geomRef} limit={count}>
+        <planeGeometry />
+        <shaderMaterial args={[shader]} side={THREE.DoubleSide} />
+        {snowflakes}
+      </Instances>
+    );
+  }, [snowflakes]);
+
+  return instances;
+};
+
+const Scene = ({
+  fields,
+  setName,
+}: {
+  fields: Model[];
+  setName: (name: string | null) => void;
+}) => {
   return (
-    <Instances ref={geomRef} limit={count}>
-      <planeGeometry />
-      <shaderMaterial args={[shader]} side={THREE.DoubleSide} />
-      {snowflakes}
-    </Instances>
+    <Canvas camera={{ position: [0, 0, 10], far: 1000 }}>
+      <OrbitControls enablePan={false} maxDistance={100} />
+      {/* <Stars /> */}
+      <Snow fields={fields} setName={setName} />
+      <ContactShadows
+        position={[0, FLOOR, 0]}
+        scale={160}
+        far={(TOP - FLOOR) / 2}
+        blur={3}
+        color="gray"
+        rotation={[-Math.PI / 2, 0, 0]}
+      />
+      <Plane
+        args={[1000, 1000]}
+        position={[0, FLOOR - 0.1, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <meshBasicMaterial color="white" />
+      </Plane>
+      <Sphere args={[160]}>
+        <meshBasicMaterial side={THREE.BackSide}>
+          <GradientTexture
+            stops={[0.4, 1]} // As many stops as you want
+            colors={["#bdd9e9", "hotpink"]} // Colors need to match the number of stops
+          />
+        </meshBasicMaterial>
+      </Sphere>
+    </Canvas>
   );
 };
 
@@ -111,42 +189,26 @@ export const getServerSideProps = async () => ({
 });
 
 export default function HTTFPage({ fields }: { fields: Model[] }) {
-  const isMobile = useIsMobile();
-
+  const [name, setName] = useState<string | null>(null);
+  const [visible, setVisible] = useState(false);
+  const setNameVisibility = useCallback((v: string | null) => {
+    if (v) {
+      setName(v);
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, []);
   return (
     <>
       <div className={styles.scene}>
-        <Canvas camera={{ position: [0, 0, 10], far: 1000 }}>
-          <OrbitControls enablePan={false} maxDistance={100} />
-          {/* <Stars /> */}
-          <Snow fields={fields} />
-          <ContactShadows
-            position={[0, FLOOR, 0]}
-            scale={160}
-            far={(TOP - FLOOR) / 2}
-            blur={3}
-            color="gray"
-            rotation={[-Math.PI / 2, 0, 0]}
-          />
-          <Plane
-            args={[1000, 1000]}
-            position={[0, FLOOR - 0.1, 0]}
-            rotation={[-Math.PI / 2, 0, 0]}
-          >
-            <meshBasicMaterial color="white" />
-          </Plane>
-          <Sphere args={[160]}>
-            <meshBasicMaterial side={THREE.BackSide}>
-              <GradientTexture
-                stops={[0.4, 1]} // As many stops as you want
-                colors={["#bdd9e9", "hotpink"]} // Colors need to match the number of stops
-              />
-            </meshBasicMaterial>
-          </Sphere>
-        </Canvas>
+        <Scene fields={fields} setName={setNameVisibility} />
       </div>
       <div className={styles.links}>
         <Link href="/gallery">back to gallery ⬅️</Link>
+        <span className={visible ? styles.nameVisible : styles.nameHidden}>
+          snowflake made by: {name}
+        </span>
       </div>
     </>
   );
