cc9b26468cdc90eaf00875ec9ae44d3496e1ad1f
Paras Sanghavi <paras@disambiguo.us>
2022-12-22 20:52:10 -0800

    better error reporting

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -15,18 +15,22 @@ export default async function handler(
   req: NextApiRequest & { body: Model },
   res: NextApiResponse
 ) {
-  const body = JSON.parse(req.body);
-  airtablePut(body);
+  try {
+    const body = JSON.parse(req.body);
+    await airtablePut(body);
 
-  const post = await createPresignedPost(s3Client, {
-    Bucket: process.env.BUCKET_NAME,
-    Key: body.file,
-    Fields: { acl: "public-read", "Content-Type": "image/png" },
-    Expires: 600, // seconds
-    Conditions: [
-      ["content-length-range", 0, 1048576], // up to 1 MB
-    ],
-  });
+    const post = await createPresignedPost(s3Client, {
+      Bucket: process.env.BUCKET_NAME,
+      Key: body.file,
+      Fields: { acl: "public-read", "Content-Type": "image/png" },
+      Expires: 600, // seconds
+      Conditions: [
+        ["content-length-range", 0, 1048576], // up to 1 MB
+      ],
+    });
 
-  res.status(200).json(post);
+    res.status(200).json(post);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 }
